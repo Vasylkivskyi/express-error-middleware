737bdec068870c70186f20b2d53c71279eed55eb
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2020-12-26 00:50:04 +0200

    add error middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const handleErrors = require('./middleware/handleErrors');
+const { BadRequest } = require('./utils/errors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,5 +12,20 @@ app.get('/', (req, res, next) => {
   res.status(200).send('Welcome to Express Middleware Demo 😄');
 });
 
+app.post('/post', async (req, res, next) => {
+  const { title, author } = req.body;
+
+  try {
+    if (!title || !author) {
+      throw new BadRequest('Missing required fields: title or author');
+    }
+    const post = await db.post.insert({ title, author });
+    res.json(post);
+  } catch (err) {
+    next(err)
+  }
+});
+
+app.use(handleErrors);
 
 app.listen(PORT, () => console.log(`Listening http://localhost:${PORT}`));
